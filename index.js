452c1cf2cc78a32d8a08b10c807aa2cb680647ce
cliff <cliffmudigo5@gmail.com>
2023-03-27 11:39:58 +0300

    partially done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const formData = {
+      name: userName,
+      email: userEmail
+    };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        document.body.append(newId);
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
+  }
--- test/indexTest.js
+++ test/indexTest.js
@@ -88,3 +88,16 @@ describe( "submitData()", () => {
       .to.include( message )
   } )
 } )
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: Steve, email: steve@steve.com.})
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message));
+}
